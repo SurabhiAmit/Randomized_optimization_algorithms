fb9e5fb8ec1825e683f24c17a446e163a2388fbe
Surabhi Chembra <svasudev3@gatech.edu>
2020-05-03 17:27:26 -0500

    Add

---
+++ Code_files/CS-7641-assignment-2/NN3.py
@@ -0,0 +1,177 @@
+
+import os
+import csv
+import time
+import sys
+sys.path.append(os.path.join(sys.path[0],'ABAGAIL','ABAGAIL.jar'))
+from func.nn.backprop import BackPropagationNetworkFactory
+from shared import SumOfSquaresError, DataSet, Instance
+from opt.example import NeuralNetworkOptimizationProblem
+from func.nn.backprop import RPROPUpdateRule, BatchBackPropagationTrainer
+import opt.RandomizedHillClimbing as RandomizedHillClimbing
+import opt.SimulatedAnnealing as SimulatedAnnealing
+import opt.ga.StandardGeneticAlgorithm as StandardGeneticAlgorithm
+from func.nn.activation import RELU
+
+# Network parameters found "optimal" in Assignment 1
+INPUT_LAYER = 16 #attribute-count
+HIDDEN_LAYER1 = 42
+OUTPUT_LAYER = 26
+TRAINING_ITERATIONS = 50001
+OUTFILE = './FINAL_NN/XXX_LOG.csv'
+
+
+def initialize_instances(infile):
+    """Read the m_trg.csv CSV data into a list of instances."""
+    instances = []
+
+    # Read in the CSV file
+    with open(infile, "r") as dat:
+        reader = csv.reader(dat)
+
+        for row in reader:
+            instance = Instance([float(value) for value in row[:-1]])
+            m = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            if row[-1] == 'A':
+                # print("A here")
+                m = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            elif row[-1] == 'B':
+                m = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            elif row[-1] == 'C':
+                m = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            elif row[-1] == 'D':
+                m = [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            elif row[-1] == 'E':
+                m = [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            elif row[-1] == 'F':
+                m = [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            elif row[-1] == 'G':
+                m = [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            elif row[-1] == 'H':
+                m = [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            elif row[-1] == 'I':
+                m = [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            elif row[-1] == 'J':
+                m = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            elif row[-1] == 'K':
+                m = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            elif row[-1] == 'L':
+                m = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            elif row[-1] == 'M':
+                m = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            elif row[-1] == 'N':
+                m = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            elif row[-1] == 'O':
+                m = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            elif row[-1] == 'P':
+                m = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            elif row[-1] == 'Q':
+                m = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            elif row[-1] == 'R':
+                m = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+            elif row[-1] == 'S':
+                m = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+            elif row[-1] == 'T':
+                m = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
+            elif row[-1] == 'U':
+                m = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
+            elif row[-1] == 'V':
+                m = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0]
+            elif row[-1] == 'W':
+                m = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
+            elif row[-1] == 'X':
+                m = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0]
+            elif row[-1] == 'Y':
+                m = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]
+            elif row[-1] == 'Z':
+                m = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+            instance.setLabel(Instance(m))
+            instances.append(instance)
+
+    return instances
+	
+
+def errorOnDataSet(network,ds,measure):
+    N = len(ds)
+    error = 0.
+    correct = 0
+    incorrect = 0
+    for instance in ds:
+        network.setInputValues(instance.getData())
+        network.run()
+        error1 = 0
+        actual = instance.getLabel()
+        predicted = network.getOutputValues()
+        #print("ACTUAL", actual)
+        #print("PREDICTED", predicted)
+        leng = actual.size()
+        actual_list=[]
+        predicted_list=[]
+        for i in range(leng):
+            diff = abs(predicted.get(i) - actual.getContinuous(i))
+            actual_list.append(actual.getContinuous(i))
+            predicted_list.append(predicted.get(i))
+            error1 += diff * diff
+        error1 = error1
+        error += error1
+        max_actual = max(actual_list)
+        max_predict = max(predicted_list)
+        actual_one = actual_list.index(max(actual_list))
+        predicted_one = predicted_list.index(max(predicted_list))
+        if actual_one == predicted_one:
+            correct += 1
+        else:
+            incorrect += 1
+    MSE = error / float(N)
+    acc = (correct / float(correct + incorrect))*100
+    return MSE, acc
+	
+	
+def train(oa, network, oaName, training_ints,validation_ints,testing_ints, measure):
+    """Train a given network on a set of instances.
+    """
+    print "\nError results for %s\n---------------------------" % (oaName,)
+    times = [0]
+    for iteration in xrange(TRAINING_ITERATIONS):
+        start = time.clock()
+        oa.train()
+        elapsed = time.clock()-start
+    	times.append(times[-1]+elapsed)
+        if iteration % 10==0:
+    	    MSE_trg, acc_trg = errorOnDataSet(network,training_ints,measure)
+            MSE_val, acc_val = errorOnDataSet(network,validation_ints,measure)
+            MSE_tst, acc_tst = errorOnDataSet(network,testing_ints,measure)
+            txt = '{},{},{},{},{},{},{},{}\n'.format(iteration, MSE_trg, MSE_val, MSE_tst, acc_trg, acc_val, acc_tst,
+                                                     times[-1]);
+            print txt
+            with open(OUTFILE.replace('XXX',oaName),'a+') as f:
+                f.write(txt)
+
+def main(P,mate,mutate):
+    """Run this experiment"""
+    training_ints = initialize_instances('m_trg.csv')
+    testing_ints = initialize_instances('m_test.csv')
+    validation_ints = initialize_instances('m_val.csv')
+    factory = BackPropagationNetworkFactory()
+    measure = SumOfSquaresError()
+    data_set = DataSet(training_ints)
+    relu = RELU()
+    rule = RPROPUpdateRule()
+    oa_name = "GA_{}_{}_{}".format(P,mate,mutate)
+    with open(OUTFILE.replace('XXX',oa_name),'w') as f:
+        f.write('{},{},{},{},{},{},{},{}\n'.format('iteration', 'MSE_trg', 'MSE_val', 'MSE_tst', 'acc_trg', 'acc_val',
+                                                   'acc_tst', 'elapsed'))
+    classification_network = factory.createClassificationNetwork([INPUT_LAYER, HIDDEN_LAYER1,OUTPUT_LAYER],relu)
+    nnop = NeuralNetworkOptimizationProblem(data_set, classification_network, measure)
+    oa = StandardGeneticAlgorithm(P, mate, mutate, nnop)
+    train(oa, classification_network, oa_name, training_ints,validation_ints, testing_ints, measure)
+        
+
+
+if __name__ == "__main__":
+    for p in [50,400]:
+        for mate in [10,50]:
+            for mutate in [5,10]:
+                args = (p,mate,mutate)
+                main(*args)
+
---
+++ README.txt
@@ -0,0 +1,79 @@
+Project Description:
+
+This project uses the randomized optimization algorithms of Random Hill Climbing, Simulated Annealing, Genetic Algorithm and MIMIC on the optimization problems of Continuous Peaks problems, Flip-flop problem and Travelling Salesman problem and to determine weights for a neural network built using backpropagation.
+
+This file contains the detailed instructions on the contents of the code and how to run the code for this project.
+
+Environment:
+
+The code was developed and exceuted in PyCharm using Jython 2.7 as interpreter and Windows 10 machine. The ABAGAIL.jar is built from the source java files and the python files are written in Jython 2.7.0.
+
+File structure:
+Inside Code_files folder, CS-7641-assignment-2 folder is stored.
+CS-7641-assignment-2 is the parent folder with the following sub-folders:
+	a.ABAGAIL folder having the jar file, ant build file and the java source files
+	b.Python files
+	c.Log files
+The contents of Python files and Log files are described later in this document. 
+
+Code files under CS-7641-assignment-2 folder:
+	
+The following python files find the optimal weights of neural network built in Assignment-1 for Letter Recognition dataset.
+1.NN0.py uses backpropagation technique
+2.NN1.py uses Random Hill Climbing
+3.NN2.py uses Simulated Annealing
+4.NN3.py uses Genetic Algorithm
+The following python files uses the randomized optimization algorithms of Random Hill Climbing, Simulated Annealing, Genetic Algorithm and MIMIC on the optimization problems.
+5.continuouspeaks.py uses all 4 randomized optimization algorithms to solve Continuous Peaks problem.
+6.flipflop.py uses all 4 randomized optimization algorithms to solve the FlipFlop problem.
+7.tsp.py uses all 4 randomized optimization algorithms to solve the Traveling Salesman problem.
+
+Log files under CS-7641-assignment-2 folder:
+
+1.Final_NN folder has the log files of NN0.py, NN1.py, NN2.py and NN3.py
+2.FinalCont folder has the log files of continuouspeaks.py
+3.FinalFF folder has the log files of flipflop.py
+4.FinalTSP folder has the log files of tsp.py
+Within these folders, the log files containing the plotting data are csv files and the file names correspond to:
+<Algorithm_used><N>_<T if applicable>_<parameters set>_LOG_<Trail_number>.csv
+N and T(if applicable) are defined for each optimization problem inside the corresponding .py file.
+Some files have <Iteration_count> added at the end of the filename and this was for experimenting individual algorithmic convergence.
+
+Datasets under CS-7641-assignment-2 folder:
+
+The parsed and preprocessed dataset is stored in the following csv files under CS-7641-assignment-2 folder:
+1.m_train.csv has the training data
+2.m_val.csv has the validation data
+3.m_test.csv has the testing data.
+The steps of preprocessing are detailed later in this README.
+
+How to run the code:
+
+1. The python files under CS-7641-assignment-2 can be run using Jython 2.7.0 interpreter.
+2. Running each of the 7 above mentioned .py files populate the log files in corresponding folders.
+ 
+Notes:
+
+ABAGAIL folder has the source files written using JAVA 1.8.0_121 and the ABAGAIL.jar. The path to ABAGAIL.jar is already included in the .py code files. 
+
+MS Excel was used to plot the charts. The data in log files were copied to MS Excel files and pivot tables were used to average the values of different runs of each parameter combination for each algorithm.
+
+Preprocessing the data using Weka 3.8 GUI:
+The preprocessing of data was done in Weka. The steps are:
+1. Download dataset from UCI repository at https://archive.ics.uci.edu/ml/datasets/letter+recognition and in Weka GUI, load dataset in Preprocess -> Open File.
+2. In Preprocess -> Edit, the correct attribute was set as the class uisng "Attribute as Class" option. 
+3. Use Filter -> Unsupervised -> Instance -> RemoveDuplicates filter to remove the duplicated rows from the dataset.
+4. Use Filter -> Unsupervised -> Instance -> Randomize filter to randomize the data using default seed value. 
+5. Using Filter -> Unsupervised -> Instance -> Resample, with no replacement, 30% of dataset is sampled and stored as testing dataset. This filter produces a random subsample of a dataset using sampling without replacement. (Used 324 as seed for Letter Recognition dataset)
+6. Setting the Invertselection parameter of Resample filter to true gives the remaining 70% of the dataset, which is stored as training dataset.
+7. From the training dataset, 10% of data is set as validation set and the rest 90% is training dataset.
+8.This 90%-10% split is done using weka-> filters -> supervised -> instance -> StratifiedRemoveFolds with fold value set as 1, numFolds as 10 and seed as 0.
+
+The preproceesed train, validation and test sets are included inside CS-7641-assignment-2 and are also available at:
+1. Letter Recognition train dataset 
+https://drive.google.com/open?id=1DTpmHsguCmWPmmez9fBFFdD8PFkbYYpv 
+2. Letter Recognition validation dataset
+https://drive.google.com/open?id=12XAmX4E5o3CASka9CWTgADI8mr2-OcLP
+3. Letter Recognition test dataset https://drive.google.com/open?id=1oi1drTqzJNdacoreVv_pkvPpcBpLFb8N 
+
+
